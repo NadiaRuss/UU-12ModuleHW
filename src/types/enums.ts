d09f2e91cc49b73011881d88bc7cd8ea944c7b2a
Надежда Яковлева <i160220@mail.ru>
2024-10-17 18:28:06 +0700

    Исправлены файлы ts

---
+++ src/types/enums.ts
@@ -0,0 +1,15 @@
+export enum NewsCategory{
+    Busines = 'business',
+    Entertaiment = 'entertaiment',
+    General = 'general',
+    Health = 'health',
+    Science = 'science',
+    Sports = 'sports',
+    Technology = 'technology'
+}
+export enum ErrorType{
+    Auth = 'auth',
+    NotFound = 'not_found',
+    Server = 'server',
+    Unknown = 'unknown'
+}
